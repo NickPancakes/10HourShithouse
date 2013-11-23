17b7af2bf894da2700247d3b23b7e284e3b3f128
Quinlan Pfiffer <qpfiffer@gmail.com>
2013-11-23 00:21:21 -0800

    More json updates to filter out videos that aren't actually ten hours.

--- scrape/smash_n_grab.py
+++ scrape/smash_n_grab.py
@@ -9,7 +9,7 @@ def main():
     output_file = open(output, 'w')
     intermediate_output_file = open('intermediate_{}'.format(output), 'a')
     urls = {}
-    for i in range(1,40):
+    for i in range(1,60):
         print "Grabbing page {}.".format(i)
         r = requests.get('http://www.youtube.com/results?search_query="10+hours"&page={}'.format(i))
         soup = BeautifulSoup(r.text)
@@ -24,11 +24,21 @@ def main():
             if len(video_time) >= 1:
                 video_time = video_time[0].text
 
-            urls[title] = { 'id': video_id, 'time': video_time }
+            try:
+                split_time = video_time.split(":")
+
+                if len(split_time) >= 3 and int(split_time[0]) >= 9:
+                    urls[title] = { 'id': video_id, 'time': video_time }
+            except IndexError:
+                print "Weird split."
+                continue
+            except AttributeError:
+                print "List split?"
+                continue
 
         intermediate_output_file.write(dumps(urls))
         print "Retrieved page {}. Sleeping.".format(i)
-        sleep(5)
+        sleep(2)
 
     output_file.write(dumps(urls))
     intermediate_output_file.close()
