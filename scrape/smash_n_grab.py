318d61bb3eb21e608b3c09b0358d3244f74c4de9
Quinlan Pfiffer <qpfiffer@gmail.com>
2013-11-22 23:10:21 -0800

    Added some logging, intermediate file. Scraped IDs.

--- scrape/smash_n_grab.py
+++ scrape/smash_n_grab.py
@@ -7,8 +7,10 @@ output = 'raw_scraped.json'
 
 def main():
     output_file = open(output, 'w')
+    intermediate_output_file = open('intermediate_{}'.format(output), 'a')
     urls = []
-    for i in range(1,2):
+    for i in range(1,35):
+        print "Grabbing page {}.".format(i)
         r = requests.get('http://www.youtube.com/results?q=10+hours&page={}'.format(i))
         soup = BeautifulSoup(r.text)
         hrefs = soup.select('#search-results h3.yt-lockup-title a.yt-uix-sessionlink')
@@ -16,9 +18,12 @@ def main():
         ids = [raw.split('=')[1] for raw in hrefs if 'watch' in raw]
 
         map(urls.append, ids)
-        sleep(10)
+        intermediate_output_file.write(dumps(list(set(urls))))
+        print "Retrieved page {}. Sleeping.".format(i)
+        sleep(5)
 
     output_file.write(dumps(list(set(urls))))
+    intermediate_output_file.close()
     output_file.close()
 
 if __name__ == '__main__':
