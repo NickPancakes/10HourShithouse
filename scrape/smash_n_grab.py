db3b4b850d13edd9e308e8ad3ca2a70fe953cbe7
Quinlan Pfiffer <qpfiffer@gmail.com>
2013-11-22 22:59:48 -0800

    Added gitignore.

--- scrape/smash_n_grab.py
+++ scrape/smash_n_grab.py
@@ -11,10 +11,14 @@ def main():
     for i in range(1,2):
         r = requests.get('http://www.youtube.com/results?q=10+hours&page={}'.format(i))
         soup = BeautifulSoup(r.text)
-        import ipdb; ipdb.set_trace()
+        hrefs = soup.select('#search-results h3.yt-lockup-title a.yt-uix-sessionlink')
+        hrefs = [test.attrs['href'] for test in hrefs]
+        ids = [raw.split('=')[1] for raw in hrefs if 'watch' in raw]
+
+        map(urls.append, ids)
         sleep(10)
 
-    output_file.write(dumps(set(urls)))
+    output_file.write(dumps(list(set(urls))))
     output_file.close()
 
 if __name__ == '__main__':
